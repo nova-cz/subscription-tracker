79c69b9930ae2101c961b55c877cd8ed9ade020e
Miguel_CR <125812157+nova-cz@users.noreply.github.com>
2025-11-02 22:58:49 -0600

    models

--- models/subscription.model.js
+++ models/subscription.model.js
@@ -13,7 +13,78 @@ const subscriptionSchema = new mongoose.Schema({
         required: [true, 'Subscription price is required'],
         min: 0,
 
+    },
+    currency: {
+        type: String,
+        enum: ['USD', 'EUR', 'GBP'],
+        default: 'USD',
+    },
+    frequency: {
+        type: String,
+        enum: ['daily', 'weekly', 'monthly', 'yearly'],
+    },
+    category: {
+        type: String,
+        enum: ['sports', 'news', 'entertainment', 'lifestyle', 'technology', 'finance', 'politics', 'other'],
+        required: true,
+    },
+    paymentMethod: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    status: {
+        type: String,
+        enum: ['active', 'canceled', 'expired'],
+        default: 'active',
+    },
+    startDate: {
+        type: Date,
+        required: true,
+        validate: {
+            validator: (value) => value < new Date(),
+            message: 'Start date must be in the past'
+        }
+    },
+    renewalDate: {
+        type: Date,
+        validate: {
+            validator: function(value){
+                return value > this.startDate();
+            },
+            message: 'Renewal date must be after the start date'
+        }
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+        index: true,
+    }
+}, {timestamps: true});
+
+//Calcula la renewalDate en caso de que el usuario no la haya metido
+subscriptionSchema.pre('save', async function (next) {
+    if(!this.renewalDate) {
+        const renewalPeriods = {
+            daily: 1,
+            weekly: 7,
+            monthly: 30,
+            yearly: 365,
+        };
+
+        this.renewalDate = new Date(this.startDate);
+        this.renewalDate.setDate(this.renewalDate.getDate() + renewalPeriods[this.frequency]);
     }
 
+    //Auto actualizar la fecha en el caso de que ya haya pasado
+    if(this.renewalDate > new Date()){
+        this.status = 'expired';
+    }
 
-}, {timestamps: true});
+    next();
+});
+
+const Subscription = mongoose.model('Subscription', subscriptionSchema);
+
+export default Subscription;
