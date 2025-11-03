f52323511224f7e6567f09ee526aaad62b6fb85b
Miguel_CR <125812157+nova-cz@users.noreply.github.com>
2025-11-03 15:07:19 -0600

    Adding middleware

--- app.js
+++ app.js
@@ -1,18 +1,29 @@
 import express from 'express';
 import {PORT} from './config/env.js';
+import cookieParser from 'cookie-parser';
 
 //Importando routes
 import authRouter from "./routes/auth.routes.js";
 import subscriptionRouter from './routes/subscription.routes.js';
 import userRouter from "./routes/user.routes.js";
 import connectionToDatabase from "./database/mongodb.js";
+import errorMiddleware from "./middlewares/error.middleware.js";
 
 const app = express();
+
+//Procesar datos JSON del req.body
+app.use(express.json());
+//Procesar datos de formularios
+app.use(express.urlencoded({ extended: false }));
+//Leer cookies enviadas por el cliente
+app.use(cookieParser());
 
 //Usas use para decirle a tu aplicacion que routes quieres usar
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/subscriptions', subscriptionRouter );
+
+app.use(errorMiddleware)
 
 app.get('/', (req, res) => {
     res.send('Welcome to the Subscription tracker API')
